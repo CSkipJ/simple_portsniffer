a1918205b619b07569c1710d2db39017fdf400d8
CSkipJ <62442845+CSkipJ@users.noreply.github.com>
2023-05-05 14:36:51 -0600

    Update portsniff.py
    Added additional CLI argument -r, --randomize which randomizes the order in which ports are sniffed. 
    Changed some logic to hopefully make things a little more performant.

--- portsniff.py
+++ portsniff.py
@@ -2,54 +2,81 @@ import argparse
 import socket
 import time
 import random
+import sys
 
-def scan_port(target_ip, port):
+
+def is_port_open(target_ip, port) -> bool:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.settimeout(1)
+    result = client.connect_ex((target_ip, port))
+    client.close()
+    return result == 0
 
-    retval = f'Port {port} is '
-    try:
-        client.connect((target_ip, port))
-        retval += 'open'
-    except socket.error:
-        retval += 'closed'
-    finally:
-        client.close()
-        return retval
 
-def arrange_ports(args):
-    target_ip = args.target_ip
-    port_range = args.port_range
-    out_file = args.out_file
-    randomize = args.randomize
-    max_delay = float(args.delay)
-
-    if '-' in port_range:
-        start_port, end_port = map(int, port_range.split('-'))
+def get_range(_port_range):
+    if '-' in _port_range:
+        return map(int, _port_range.split('-'))
     else:
-        start_port = int(port_range)
-        end_port = int(port_range)
+        return (int(_port_range), int(_port_range))
 
-    if not out_file:
-        for port in range(start_port, end_port+1):
-            print(scan_port(target_ip, port))
-            if max_delay:
-                time.sleep(random.uniform(0.1, max_delay))
-    else: 
-        with open(out_file, 'w') as f:
-            for port in range(start_port, end_port+1):
-                f.write(scan_port(target_ip, port))
-                if max_delay:
-                    time.sleep(random.uniform(0.1, max_delay))
+
+def get_ports(_port_range) -> list:
+    ranges = _port_range.split(',')
+
+    _ports_list = []
+    for rng in ranges:
+        _start_port, _end_port = get_range(rng)
+        for port in range(_start_port, _end_port+1):
+            _ports_list.append({'port':port, 'is_open':None})
+    return _ports_list
+
+
+def sequential_scan(args):
+    for port in get_ports(args.port_range):
+        if is_port_open(args.target_ip, port['port']):
+            port['is_open'] = 'open'
+            print(f"Port {port['port']} is {port['is_open']}")
+        elif args.all:
+            port['is_open'] = 'closed'
+            print(f"Port {port['port']} is {port['is_open']}")
+        time.sleep(random.uniform(0.01, float(args.delay)))
+
+
+def random_scan(args):
+    ports = get_ports(args.port_range)
+    rports = ports.copy()
+    random.shuffle(rports)
+
+    for port in rports:
+        if is_port_open(args.target_ip, port['port']):
+            port['is_open'] = 'open'
+            print(f"Port {port['port']} is {port['is_open']}")
+        elif args.all:
+            port['is_open'] = 'closed'
+            print(f"Port {port['port']} is {port['is_open']}")
+        time.sleep(random.uniform(0.01, float(args.delay)))
+
+
+def main(args):
+    if not args.randomize:
+        sequential_scan(args)
+    else:
+        random_scan(args)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Port scanner")
     parser.add_argument("target_ip", type=str, help="IP address to scan")
     parser.add_argument("port_range", type=str, help="Port range to scan (e.g. 1-65535). Also accepts a single port.")
     parser.add_argument("-o", "--out_file", type=str, help="The destination file for scanned port information.")
-    parser.add_argument("-r", "--randomize", action='store_true', help="Will randomize the order in which ports are scanned. Not implemented.")
-    parser.add_argument("-d", "--delay", help="Turns on randomized delay, where the max possible delay value is the value given.")
+    parser.add_argument("-r", "--randomize", action='store_true', help="Will randomize the order in which ports are scanned.")
+    parser.add_argument("-d", "--delay", default=0.01, help="Turns on randomized delay, where the max possible delay value is the value given.")
+    parser.add_argument("-a", "--all", action='store_true', help="Prints every port scanned. If not used only open ports will be printed.")
 
     args = parser.parse_args()
 
-    arrange_ports(args)
+    if args.out_file:
+        with open(args.out_file, 'w') as sys.stdout:
+            main(args)
+    else:
+        main(args)
